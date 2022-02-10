a413d3305a27db2a95b7bb60baf8e8a756127f6e
nGyu <vpdls1511@gmail.com>
2022-02-10 22:33:52 +0900

    Math_1987

---
+++ src/acmicpc/math/icpc_1978.java
@@ -0,0 +1,27 @@
+package acmicpc.math;
+
+import java.util.Scanner;
+
+public class icpc_1978 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        int count = 0;
+
+        for(int i = 0 ; i < N ; i++){
+            int cnt = in.nextInt();
+            count += cnt(cnt);
+        }
+
+        System.out.println(count);
+    }
+
+    public static int cnt (int num) {
+        if(num == 1) return 0;
+        for(int i = 2 ; i < num ; i++){
+            if(num % i == 0) return 0;
+        }
+        return 1;
+    }
+}
