c2a7db566d17a0ec8638b2b1c1ff418977bebb26
nGyu <vpdls1511@gmail.com>
2022-02-13 12:28:54 +0900

    Math_10757

---
+++ src/acmicpc/math/icpc_10757.java
@@ -0,0 +1,17 @@
+package acmicpc.math;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class icpc_10757 {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        BigInteger a = new BigInteger(in.next());
+        BigInteger b = new BigInteger(in.next());
+
+        a = a.add(b);
+
+        System.out.println(a.toString());
+    }
+}
