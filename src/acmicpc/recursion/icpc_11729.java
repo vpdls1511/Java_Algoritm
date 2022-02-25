9259405d0fdd10cb563431c16f17f976894104f2
nGyu <vpdls1511@gmail.com>
2022-02-25 21:43:30 +0900

    Recutsion_icpc_11729_review

--- src/acmicpc/recursion/icpc_11729.java
+++ src/acmicpc/recursion/icpc_11729.java
@@ -11,21 +11,18 @@ public class icpc_11729 {
 
         sb.append((int) Math.pow(2,N)-1).append("\n");
 
+
         hanoi(N , 1,2,3);
         System.out.print(sb);
     }
 
     private static void hanoi(int n , int start, int mid, int to){
-        //
-        // 2
-        // 3   1
-        // 1 2 3
         if(n == 1){
-            sb.append(start+" " + to+"\n");
-            return ;
+            sb.append(start + " " + to + "\n");
+            return;
         }
-        hanoi(n-1 , start, to, mid);
-        sb.append(start+" "+to +"\n");
-        hanoi(n-1 , mid, start, to);
+        hanoi(n-1, start, to , mid);
+        sb.append(start + " " + to + "\n");
+        hanoi(n-1, mid, start, to);
     }
 }
