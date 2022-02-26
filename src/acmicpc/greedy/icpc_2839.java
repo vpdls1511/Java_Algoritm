21e98d697ba98fc81cbd2198da99881a0113c310
nGyu <vpdls1511@gmail.com>
2022-02-26 13:45:45 +0900

    greedy solution()

--- src/acmicpc/greedy/icpc_2839.java
+++ src/acmicpc/greedy/icpc_2839.java
@@ -28,4 +28,18 @@ public class icpc_2839 {
         System.out.println(bag);
 
     }
+
+    // 다른사람의 풀이
+    private static void solution(int n){
+        // https://st-lab.tistory.com/72
+        if(n == 4 || n == 7){
+            System.out.println(-1);
+        }else if( n%5 == 0){
+            System.out.println(n/5);
+        }else if( n%5 == 1 || n%5 == 3){
+            System.out.println((n/5) + 1);
+        }else if( n%5 == 2 || n%5 == 4){
+            System.out.println((n/5) + 2);
+        }
+    }
 }
