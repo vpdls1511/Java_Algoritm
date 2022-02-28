258db3e3b8515cb2d2590a8ed50539b9e8a151e1
nGyu <vpdls1511@gmail.com>
2022-02-28 13:39:56 +0900

    dp_2293

--- src/acmicpc/dp/icpc_2293.java
+++ src/acmicpc/dp/icpc_2293.java
@@ -3,10 +3,11 @@ package acmicpc.dp;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class icpc_2293 {
+
+    // TODO : 꼭 다시 정리 해보기
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,22 +15,21 @@ public class icpc_2293 {
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n+1];
-        int[][] dp = new int[n+1][k+1];
+        int[] arr = new int[n];
+        int[] dp = new int[k+1];
 
         for(int i = 0 ; i < n ; i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(arr);
-        dp[1][0] = arr[1];
+        dp[0] = 1;
 
-        for(int coin = 1 ; coin <= n ; coin++){
-            for(int value = coin ; value <= k ; value++){
-                dp[coin][value] =  dp[coin][value - coin] + dp[coin-1][value];
+        for(int i = 0; i < n; i++) {
+            for(int j = arr[i]; j <= k; j++) {
+                dp[j] += dp[j - arr[i]];
             }
         }
 
-        System.out.println(dp[n][k]);
+        System.out.println(dp[k]);
     }
 }
